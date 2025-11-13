1fb22bf4bfe98e0916e332e2e9d2683a17ce15c2
PHVIEIRA3006 <vieirap@ufba.br>
2025-11-13 10:32:53 -0300

    a

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 #URL DO SITE https://meu-primeiro-app-gdiwysr9j84rne6pvvgh4f.streamlit.app/
 st.title('Meu primeiro App🎶')
 st.header('Vamos fazer algo com intertividade')
-n = st.number_imput('Entre com um numero')
+n = st.number_input('Entre com um numero')
 st.write(f'O numero que você escolheu ao quadrado é {n**2}')

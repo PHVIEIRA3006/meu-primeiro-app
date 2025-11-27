a41422450936a11f26b25edfa42ec0a4e40ab915
PHVIEIRA3006 <vieirap@ufba.br>
2025-11-27 10:13:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 #URL DO SITE https://meu-primeiro-app-phvs3006.streamlit.app/
+#URL DO KAGGLE: https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd

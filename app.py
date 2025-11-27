987ae84a7f4b978ef9c076f0450cc6527038f414
PHVIEIRA3006 <vieirap@ufba.br>
2025-11-27 09:09:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#URL DO SITE https://meu-primeiro-app-gdiwysr9j84rne6pvvgh4f.streamlit.app/
+#URL DO SITE https://meu-primeiro-app-phvs3006.streamlit.app/
 st.title('Meu primeiro App🎶')
 st.header('Vamos fazer algo com intertividade')
 n = st.number_input('Entre com um numero')

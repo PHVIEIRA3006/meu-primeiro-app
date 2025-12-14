39bbab066ae787f5154c809fcc6522289aeff0c2
PHVIEIRA3006 <vieirap@ufba.br>
2025-12-13 23:08:51 -0300

    Add print statement for loaded data in app.py
    Added print statement to display the loaded data.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 #URL DO SITE https://meu-primeiro-app-phvs3006.streamlit.app/
 #URL DO KAGGLE: https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce
 import streamlit as st
@@ -6,3 +8,4 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # 1. Carregar dados (sem cache, direto)
 base = pd.read_csv('teste3_filtered.csv')
+print(base)
